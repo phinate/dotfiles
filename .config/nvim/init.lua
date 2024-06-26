caf182ad70f6b8d633495f9b5cb03e6ad4b460d6
Nathan Simpson <nsimpson@turing.ac.uk>
2024-06-26 14:10:23 +0100

    more minimal config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,17 +35,6 @@ require("lazy").setup({
   { 
       "ellisonleao/gruvbox.nvim", priority = 1000 , config = true
   },
-  -- Telescope for fuzzy finding
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Find files' })
-      vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Live grep' })
-    end,
-  },
-
   -- Surround plugin
   "tpope/vim-surround",
 
@@ -106,92 +95,6 @@ require("lazy").setup({
       })
     end,
   },
-
-  -- Markdown support
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
-  {
-    "dhruvasagar/vim-table-mode",
-    ft = { "markdown" },
-  },
-  -- {
-  --   "preservim/vim-markdown",
-  --   ft = { "markdown" },
-  --   dependencies = { "godlygeek/tabular" },
-  -- },
-  { -- quarto!
-    'quarto-dev/quarto-nvim',
-    ft = { 'quarto' },
-    dev = false,
-    opts = {
-      lspFeatures = {
-        languages = { 'python'},
-      },
-      codeRunner = {
-        enabled = true,
-        default_method = 'slime',
-      },
-    },
-  },
-
-  { -- directly open ipynb files as quarto docuements
-    -- and convert back behind the scenes
-    'GCBallesteros/jupytext.nvim',
-    opts = {
-      custom_language_formatting = {
-        python = {
-          extension = 'qmd',
-          style = 'quarto',
-          force_ft = 'quarto',
-        },
-      },
-    },
-  },
-
-  { -- paste an image from the clipboard or drag-and-drop
-    'HakonHarnes/img-clip.nvim',
-    event = 'BufEnter',
-    ft = { 'markdown', 'quarto', 'latex' },
-    opts = {
-      default = {
-        dir_path = 'img',
-      },
-      filetypes = {
-        markdown = {
-          url_encode_path = true,
-          template = '![$CURSOR]($FILE_PATH)',
-          drag_and_drop = {
-            download_images = false,
-          },
-        },
-        quarto = {
-          url_encode_path = true,
-          template = '![$CURSOR]($FILE_PATH)',
-          drag_and_drop = {
-            download_images = false,
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require('img-clip').setup(opts)
-      vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
-    end,
-  },
-
-  { -- preview equations
-    'jbyuki/nabla.nvim',
-    keys = {
-      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
-    },
-  },
 })
 
 -- Language-specific settings
@@ -200,23 +103,6 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.bo.tabstop = 2
     vim.bo.shiftwidth = 2
-  end,
-})
-
--- Markdown-specific settings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    -- Enable spell checking for Markdown files
-    vim.opt_local.spell = true
-    vim.opt_local.spelllang = "en_us"
-
-    -- Set textwidth for automatic line breaks
-    vim.opt_local.textwidth = 80
-
-    -- Keymaps for Markdown
-    vim.keymap.set('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', { buffer = true, desc = 'Toggle Markdown Preview' })
-    vim.keymap.set('n', '<leader>mt', ':TableModeToggle<CR>', { buffer = true, desc = 'Toggle Table Mode' })
   end,
 })
 
