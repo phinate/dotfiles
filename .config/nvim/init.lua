8f76d0acb06624f34a6a6943416206715dd6c0d3
Nathan Simpson <phinate@protonmail.com>
2025-08-01 23:56:18 +0100

    rewrite of nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,131 +1,82 @@
--- Basic settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.smartindent = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.termguicolors = true
+-- style & admin
+vim.o.termguicolors = true
+vim.o.tabstop = 4
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
+vim.o.signcolumn = "yes"
 
--- Set leader key
+-- leader & keybinds
 vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"*y<CR>')
 
--- Basic keymaps
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
-vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', { desc = 'Quit' })
+-- don't start new comment paragraph with 'o' or 'O'
+-- when in comment context (enter still works)
+-- okay apparently this needs to be sourced to work? idk...
+vim.opt.formatoptions:remove({ "o" })
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- plugins
+vim.pack.add({
+		{ src = "https://github.com/ellisonleao/gruvbox.nvim"},
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+})
 
--- Plugin specifications
-require("lazy").setup({
-  { 
-      "ellisonleao/gruvbox.nvim", priority = 1000 , config = true
-  },
-  -- Surround plugin
-  "tpope/vim-surround",
+-- lsp
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
 
-  -- Commentary plugin
-  "tpope/vim-commentary",
-
-  -- Quarto
-  {
-    "quarto-dev/quarto-nvim",
-    dependencies = {
-      "jmbuhr/otter.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        require('quarto').setup({
-        -- Basic configuration
-        lspFeatures = {
-          enabled = true,
-          languages = { 'r', 'python', 'julia' },
-          diagnostics = {
-            enabled = true,
-          }, 
-        }})
+-- ruff
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
     end
-  },
-
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "haskell", "markdown", "markdown_inline", "latex" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      })
-    end,
-  },
-
-  -- LSP Support
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "pyright", "hls", "lua_ls", "rust_analyzer" },
-        automatic_installation = true,
-      })
-
-      local lspconfig = require("lspconfig")
-      lspconfig.pyright.setup{}
-      lspconfig.hls.setup{}
-
-
-      -- Global mappings
-      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        end,
-      })
-    end,
-  },
-})
-
--- Language-specific settings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "haskell",
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
   end,
+  desc = 'LSP: Disable hover capability from Ruff',
 })
 
-vim.cmd([[colorscheme gruvbox]])
+require('lspconfig').pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+}
 
+require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+		ensure_installed = { "python" },
+		highlight = { enable = true }
+})
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>L', vim.lsp.buf.format)
+
+vim.lsp.enable({ "lua_ls", "pyright", "ruff" })
+require "gruvbox".setup({transparent_mode = true })
+vim.cmd("colorscheme gruvbox")
